d237c8afa6acf23cde4b33b78db3ce4a509e157c
Emilien Cosson <emilien@em-ilien.fr>
2024-05-25 15:09:03 +0200

    add html/css template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Clock from './Clock.svelte';
+</script>
+
+<section class="app">
+	<Clock />
+</section>
+
+<style>
+	:global(body),
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	.app {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		width: 100vw;
+	}
+</style>
---
+++ src/routes/Clock.svelte
@@ -0,0 +1,100 @@
+<script>
+	let secondsHandTransform = 0;
+	setInterval(() => {
+		secondsHandTransform += 6;
+	}, 1000);
+</script>
+
+<div class="clock-ctn">
+	<div class="clock">
+		<div class="seconds-hand-ctn hand-ctn" style:transform={`rotate(${secondsHandTransform}deg)`}>
+			<div class="seconds-hand hand"></div>
+		</div>
+		<div
+			class="minutes-hand-ctn hand-ctn"
+			style:transform={`rotate(${secondsHandTransform / 60}deg)`}
+		>
+			<div class="minutes-hand hand"></div>
+		</div>
+		<div
+			class="hours-hand-ctn hand-ctn"
+			style:transform={`rotate(${secondsHandTransform / 3600}deg)`}
+		>
+			<div class="hours-hand hand"></div>
+		</div>
+		<div class="center"></div>
+	</div>
+</div>
+
+<style>
+	.clock-ctn {
+		width: calc(min(100vh, 100vw) - 1em);
+		height: calc(min(100vh, 100vw) - 1em);
+	}
+
+	.clock {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 3rem;
+		font-family: monospace;
+		border-radius: 50%;
+		border: 5px solid black;
+		width: calc(100% - 2 * 5px);
+		height: calc(100% - 2 * 5px);
+	}
+
+	.hand-ctn {
+		height: calc(min(100vh, 100vw));
+		display: flex;
+		justify-content: center;
+		align-items: flex-start;
+		position: absolute;
+	}
+
+	.hand {
+		width: 50%;
+		border-radius: 16px;
+	}
+
+	.seconds-hand-ctn {
+		background-color: #ff000005;
+		width: 20px;
+	}
+
+	.seconds-hand {
+		background-color: rgb(255, 0, 0);
+		height: 50%;
+		transform: translateY(calc(0% + 0.5em));
+	}
+
+	.minutes-hand-ctn {
+		background-color: #00ff0005;
+		width: 30px;
+	}
+
+	.minutes-hand {
+		background-color: #000000;
+		height: 40%;
+		transform: translateY(calc(20% + 0.5em));
+	}
+
+	.hours-hand-ctn {
+		background-color: #0000ff05;
+		width: 40px;
+	}
+
+	.hours-hand {
+		background-color: #000000;
+		height: 30%;
+		transform: translateY(calc(60% + 0.5em));
+	}
+
+	.center {
+		width: 0.75em;
+		height: 0.75em;
+		background-color: #000000;
+		border-radius: 50%;
+		position: absolute;
+	}
+</style>
